e0839da19418d6dc19b2cbeefb397ef094435077
JablonskiFlp <146426227+JablonskiFlp@users.noreply.github.com>
2024-04-30 16:19:08 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
 from flask import Flask, render_template
+from database import engine, load_jobs_from_db
+from sqlalchemy import  text
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1, 
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 1,00,000'
-  },
-  {
-    'id': 2, 
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3, 
-    'title': 'Front-end engineer',
-    'location': 'Remote',
-    'salary': 'Rs. 12,00,000'
-  },
-  {
-    'id': 4, 
-    'title': 'Back-end engineer',
-    'location': 'Remote',
-    'salary': '$12,000'
-  }
-]
+
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html", jobs=JOBS, company_name = 'Jovian')
+  jobs = load_jobs_from_db()
+  return render_template("home.html", jobs=jobs, company_name = 'FLPS')
+
+# @app.route("/api/jobs"):
+# def list_jobs():
+#   return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True)
+
