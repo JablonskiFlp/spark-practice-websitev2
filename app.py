f4e63ba0cdcf099f116e889c936940effbc4ec46
JablonskiFlp <146426227+JablonskiFlp@users.noreply.github.com>
2024-05-08 21:19:42 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from database import engine, load_jobs_from_db
+from flask import Flask, render_template, jsonify
+from database import engine, load_jobs_from_db, load_job_from_db
 from sqlalchemy import  text
 
 app = Flask(__name__)
@@ -15,6 +15,13 @@ def hello_world():
 # def list_jobs():
 #   return jsonify(JOBS)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('job_page.html', job = job)
+  
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True)
 
