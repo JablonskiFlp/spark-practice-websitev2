cc20f0b4dcdb334d45c461d20788d971643ef333
JablonskiFlp <146426227+JablonskiFlp@users.noreply.github.com>
2024-05-09 10:52:07 +0000

    store applications in db

--- database.py
+++ database.py
@@ -1,22 +1,20 @@
 from sqlalchemy import create_engine, text
 import os
+
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  }
-)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
-with engine.connect() as conn:
-  result = conn.execute(text("select * from jobs"))
-  result_dicts = []
-  for row in result.all():
-    result_dicts.append(row._mapping)
-  print(result_dicts)
+# with engine.connect() as conn:
+#   result = conn.execute(text("select * from jobs"))
+#   result_dicts = []
+#   for row in result.all():
+#     result_dicts.append(row._mapping)
+#   print(result_dicts)
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
@@ -26,11 +24,10 @@ def load_jobs_from_db():
       jobs.append(row._mapping)
     return jobs
 
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-       text(f"SELECT * FROM jobs WHERE id={id}")
-      )
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
     rows = []
     for row in result.all():
       rows.append(row._mapping)
@@ -38,4 +35,11 @@ def load_job_from_db(id):
       return None
     else:
       return rows
-    
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin_url']}', '{data['education']}', '{data['work_experience']}', '{data['resume_url']}')")
+    conn.execute(query)
+    conn.commit()
